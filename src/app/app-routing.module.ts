575ec95d25264aa8bb9f4a2b0ef941f72eb1044c
GKDigitalHut <gokul.krishnan@gkdigitalhut.com>
2023-12-17 22:09:19 +0530

    Added login and signup form html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,12 @@ import { CommonFormComponent } from './common-form/common-form.component';
 import { CommonTableComponent } from './common/common-table/common-table.component';
 import { EntityComponent } from './entity/entity.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'sign-up',component:SignUpComponent},
   {path:'home',component:HomeComponent},
   {path:'vehicleDetails',component:CommonTableComponent},
   {path:'bookingForm',component:CommonFormComponent},
---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sign-up',
+  standalone: true,
+  imports: [],
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.scss'
+})
+export class SignUpComponent {
+
+  constructor(private _router:Router){}
+
+  navigateToLogin(){
+    this._router.navigate(['login'])
+  }
+
+}
