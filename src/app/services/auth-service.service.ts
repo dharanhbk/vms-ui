83c1f44615eba01e7237680267d7e32f89a18689
GKDigitalHut <gokul.krishnan@gkdigitalhut.com>
2023-12-19 21:57:09 +0530

    Integrated login API

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -14,9 +14,6 @@ export class AuthServiceService {
 
    authorize(){
     console.log("Came to service method")
-    return this.http.get(`${this.authUrl}/oauth2/authorize?code_challenge=Lb_adNlATEOgzh4VMJzrVslng47fodO8oNzKj9_BTNE&client_id=gokul-client&code_challenge_method=S256&response_type=code&redirect_uri=http://127.0.0.1:4200/login/oauth2/code/gokul-client&scope=openid`)
-  }
-  login(){
-    // return 
+    return this.http.get(`${this.authUrl}/oauth2/authorize?code_challenge=Lb_adNlATEOgzh4VMJzrVslng47fodO8oNzKj9_BTNE&client_id=gokul-client&code_challenge_method=S256&response_type=code&redirect_uri=http://localhost:4200/home`)
   }
 }
