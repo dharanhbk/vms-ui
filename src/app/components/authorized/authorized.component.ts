fc386c0984c93b89afcd1dad599a7a567390298b
gokulkrishnan712 <kndharanidharan@gmail.com>
2023-12-21 18:47:45 +0530

    auth added

---
+++ src/app/components/authorized/authorized.component.ts
@@ -0,0 +1,45 @@
+import { TokenService } from './../../services/token.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth-service.service';
+
+@Component({
+  selector: 'app-authorized',
+  templateUrl: './authorized.component.html',
+  styleUrls: ['./authorized.component.css']
+})
+export class AuthorizedComponent implements OnInit {
+
+  code = '';
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private authService: AuthService,
+    private tokenService: TokenService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe( data => {
+      this.code = data['code'];
+      const code_verifier = this.tokenService.getVerifier();
+      this.tokenService.deleteVerifier();
+      this.getToken(this.code, code_verifier);
+    });
+  }
+
+  getToken(code: string, code_verifier: string): void {
+    this.authService.getToken(code, code_verifier).subscribe(
+      data => {
+        console.log("test get token");
+       this.tokenService.setTokens(data.access_token, data.refresh_token);
+       console.log(data.access_token+" \n REFERSH_TOKEN :"+data.refresh_token);
+       this.router.navigate(['']);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+}
---
+++ src/app/sso.config.ts
@@ -0,0 +1,11 @@
+// import { AuthConfig } from "angular-oauth2-oidc";
+
+// export const authConfig:AuthConfig = {
+
+//     issuer:"http://localhost:9000/oauth2/authorize",
+//     // grantType:"authorization_code",
+//     redirectUri:window.location.origin +'/index.html',
+//     clientId:'gokul-client',
+//     scope:'openid'
+
+// }
